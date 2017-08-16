609addda768a13405c633dc6f26c52b917c5ed77
bumbacoin <bumbacoin@gmail.com>
2017-08-16 09:27:14 +0800

    isolate from main chain clients

--- src/version.h
+++ src/version.h
@@ -30,19 +30,19 @@ static const int DATABASE_VERSION = 80000;
 // vchBlockSig field added to CBlock  1000000
 
 static const int POW_CLIENT_VERSION = 1010000;
-static const int POW_PROTOCOL_VERSION = 80002;
+static const int POW_PROTOCOL_VERSION = 99990;
 static const int POW_TX_VERSION = 3;
 static const int POW_BLOCK_VERSION = 2;
 
 // network protocol versioning
 //
-static const int PROTOCOL_VERSION = 80100;
+static const int PROTOCOL_VERSION = 99991;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 80002;
+static const int MIN_PEER_PROTO_VERSION = 99990;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -50,7 +50,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 32400;
+static const int NOBLKS_VERSION_END = 89999;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
